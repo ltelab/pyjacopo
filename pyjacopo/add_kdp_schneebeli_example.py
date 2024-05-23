9cd26ca70cb9f2dbf5857b2dad1d8d9ccecbf757
annecbroux <ac.billault-roux@wanadoo.fr>
2024-05-23 14:43:52 +0200

    added some comments

--- pyjacopo/add_kdp_schneebeli_example.py
+++ pyjacopo/add_kdp_schneebeli_example.py
@@ -20,18 +20,21 @@ warnings.filterwarnings('ignore')
 
 config  = parse_config('config_spec.yml')
 out_dir = '/ltenas8/anneclaire/ICEGENESIS_2021/MXPol/Proc_data_w_Kdp/
-SIZE_THRES = 1e7
 
-for raw in sorted(glob.glob('/ltedata/ICEGENESIS_2021/Radar/2021-01-26*/XPOL-*.dat')):
+SIZE_THRES = 1e7 # just to find DPP vs. FFT to name the files accurately
+RHOHV_THRES = 0.5 # the appropriate threshold might vary a little depending on the data we are using...
+SNRH_THRES = 0 # same here
+
+for raw in sorted(glob.glob('/ltedata/ICEGENESIS_2021/Radar/*/XPOL-*.dat')):
     name = raw.split('/')[-1][:-4]
    if os.path.getsize(raw) > SIZE_THRES:
         add = 'FFT'
     else:
         add = 'DPP'
     
-    outnameRHI = out_dir+'+name+'_'+add+'_RHI_Kdp.nc'
+    outnameRHI = out_dir+name+'_'+add+'_RHI_Kdp.nc'
    
-    
+
     if (os.path.exists(outnameRHI)) | (os.path.exists(outnamePPI)):
         continue
     
@@ -39,11 +42,13 @@ for raw in sorted(glob.glob('/ltedata/ICEGENESIS_2021/Radar/2021-01-26*/XPOL-*.d
         header, records = read_raw_data(raw, config)    
         if 'RHI' in records: # NB in this campaign, PPI are only vertical -> no Kdp
 
+            # We need to filter the data based on Rhohv and SNRh thresholds
             radar = process_dataset('RHI',header,records['RHI'],config)
             radar.fields['Psidp']['data'].mask=False
-            radar.fields['Psidp']['data'].mask[radar.fields['Rhohv']['data'].data<0.5]=True
-            radar.fields['Psidp']['data'].mask[radar.fields['SNRh']['data'].data<0]=True
-
+            radar.fields['Psidp']['data'].mask[radar.fields['Rhohv']['data'].data<RHOHV_THRES]=True
+            radar.fields['Psidp']['data'].mask[radar.fields['SNRh']['data'].data<SNRH_THRES]=True
+            
+            # The following computes Kdp according to Schneebeli et al. 2014
             radar.fields['Kdp'] = pyart.retrieve.kdp_schneebeli(radar,psidp_field='Psidp',band='X',prefilter_psidp=True,filter_opt={'rhohv_field':'Rhohv'})[0] 
             azmean = radar.azimuth['data'].mean()
             write_cfradial(outnameRHI,radar)
